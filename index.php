ca6e4af059d253289016de0b785743f0b75948c2
soburi59 <591959ant@gmail.com>
2023-04-14 16:08:19 +0900

    add php

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
-  $pdo = new PDO("mysql:dbname=blog", "root");
-  $st = $pdo->query("SELECT * FROM post ORDER BY no DESC");
-  $posts = $st->fetchAll();
+  $pdo = new PDO("mysql:dbname=blog", "root");#データベース全体を取得
+  $st = $pdo->query("SELECT * FROM post ORDER BY no DESC");#postを取得
+  $posts = $st->fetchAll();#postを配列に格納
   for ($i = 0; $i < count($posts); $i++) {
     $st = $pdo->query("SELECT * FROM comment WHERE post_no={$posts[$i]['no']} ORDER BY no DESC");
-    $posts[$i]['comments'] = $st->fetchAll();
+    #post_noが一致するコメントをすべて取得
+    $posts[$i]['comments'] = $st->fetchAll();#postsの配列に入れる
   }
   require 't_index.php';
 ?>
