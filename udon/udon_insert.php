39a8edffe732199bc12e812f0fd2412b8bf75eeb
soburi59 <591959ant@gmail.com>
2023-04-17 16:19:32 +0900

    文字化けをなおしました　

--- udon/udon_insert.php
+++ udon/udon_insert.php
@@ -1,4 +1,11 @@
-<?php
+<form action="udon_insert.php" method="post">
+    名前<br>
+    <input type="text" name="name"><br>
+    価格<br>
+    <input type="text" name="price"><br>
+    <input type="submit">
+  </form>
+  <?php
   $pdo = new PDO("mysql:dbname=test2", "root");
   $st = $pdo->prepare("INSERT INTO udon VALUES(?,?)");
   $st->execute(array($_POST['name'], $_POST['price']));
